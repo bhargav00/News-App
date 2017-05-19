f04a62fe40a51e4f055cb3464547cc5d46608d7f
bhargav00 <bmaniar00@gmail.com>
2017-05-19 19:08:29 +0530

    Read More Done

--- src/components/news.component.js
+++ src/components/news.component.js
@@ -2,6 +2,17 @@ import React from 'react';
 import Slider from 'react-slick';
 import NewsItem from './newsitem.component';
 export default class NewsComponent extends React.Component {
+       constructor(props) {
+            super(props);
+             this.state = {
+                news_id:''
+            };
+        }
+        ReadMoreHandler(news_id){
+              this.setState({
+            news_id:news_id
+        })  
+        }
         render() {
             const settings = {
                 dots: false,
@@ -21,45 +32,64 @@ export default class NewsComponent extends React.Component {
                 //     console.log('after change', currentSlide);
                 //   },
             };
+            
             var news=[
                 {
-                news_id:1,
+                news_id:0,
                 title:"Dark Horse",
                 image:require("../images/news1.jpg"),
                 category:"fashion",
                 s_description:"While I know ‘Dark Horse’ is supposed to invoke a sense of poetry or romance, I’ve been feeling quite like a horse and cat both are friends ...",
                 post_date:"19-Sept",
-                readers:20
+                readers:20,
+                bookmarks:10,
+                author:"Shivya Nath",
+                author_img:require("../images/author1.jpg"),
+                author_subscribers:20
             }
             ,{
-                news_id:2,
+                news_id:1,
                 title:" What to wear to an outdoor Indian wedding",
                 image:require("../images/news2.jpg"),
                 category:"fashion",
                 s_description:"So the wedding planning has finally started and step one is to decide on a location. We are still in the process of...",
                 post_date:"02-Oct",
-                readers:3
+                readers:3,
+                bookmarks:20,
+                author:"Pooja M",
+                author_img:require("../images/author1.jpg"),
+                author_subscribers:22
             }
             ,{
-                news_id:3,
+                news_id:2,
                 title:"That Left Me Scared Shitless.",
                 image:require("../images/news3.jpg"),
                 category:"travel",
                 s_description:"Every once in a while, I look back at my travels and think of the moments that have changed something in me.",
                 post_date:"21-Nov",
-                 readers:40
+                readers:40,
+                bookmarks:13,
+                author:"Shivya Nath",
+                author_img:require("../images/author1.jpg"),
+                author_subscribers:20
                 }
             ];
-            var itemList = [];
-            news.forEach(function(item){
-            itemList.push( <div><NewsItem news={item} /><div className="black">aa</div></div>)
-            })
+            var newsList = [];
+            news.forEach(function(item,index){
+            newsList.push( <div key={index}><NewsItem news={item} ReadMoreHandler={this.ReadMoreHandler.bind(this)}/><div className="black"></div></div>)
+            },this);
+            var view;
+            if(this.state.news_id==''){
+                view=<Slider {...settings}>{newsList}</Slider>;
+            }
+            else{
+                 view=<NewsItem news={news[this.state.news_id]}/>;
+            }
             return ( 
                         <div className="col-sm-3 col-xs-12 NewsComponent">
-                             <Slider {...settings}>
-                               {itemList}
-                               
-                            </Slider>
+                             {
+                              view  
+                             }
                         </div>
                  )
             }
--- src/components/newsitem.component.js
+++ src/components/newsitem.component.js
@@ -10,20 +10,20 @@ export default class NewsItem extends React.Component {
                            <div className="contentBox">
                                <div className="category">{this.props.news['category']}</div>
                                 <h2 className="title">{this.props.news['title']}</h2>
-                                <div className="infoBar">{this.props.news['post_date']}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;6 min read&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{this.props.news['readers']} Readers</div>
+                                <div className="infoBar">{this.props.news['post_date']}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{this.props.news['readers']} Readers</div>
                                 <p className="sDescription">{this.props.news['s_description']}</p>
                                 <div>
-                                    <a className="readMore">Read more...</a>
+                                    <a className="readMore" onClick={()=>this.props.ReadMoreHandler(this.props.news['news_id'])}>Read more...</a>
                                     <a className="bookmarkBar">
                                         <i className="fa fa-bookmark-o"></i>
-                                        <span className="bookmarks">20 bookmarks</span>
+                                        <span className="bookmarks">{this.props.news['bookmarks']} bookmarks</span>
                                     </a>
                                 </div>
                                  <hr/>
                            <div className="footerBar">
-                                        <img className="footerMedia" width="40" height="40" src="https://theshootingstar.files.wordpress.com/2014/08/shivya-nath.jpg" alt=""/>                            
-                                        <span className="author">Shivya Nath</span>
-                                        <span className="subscriber">19 subscribers</span>
+                                        <img className="footerMedia" width="40" height="40" src={this.props.news['author_img']} alt=""/>                            
+                                        <span className="author">{this.props.news['author']}</span>
+                                        <span className="subscriber">{this.props.news['author_subscribers']} subscribers</span>
                                 </div>
                            </div>
                           
